aea5ed40aa60d583c21fe806ce23f09e3417bf9c
Joshua Barrett <jjbarr.public@gmail.com>
2021-10-27 09:01:40 -0400

    minor formatting changes

--- mpk.c
+++ mpk.c
@@ -29,9 +29,9 @@ uint64_t MPK_UnsafeBytesToNum(uint8_t *v, size_t bytes){
     memcpy(&rv.b, v, bytes);
 #ifdef LITTLE_ENDIAN
     for(int i = 0; i < bytes / 2; i++){
-	uint8_t tmp = rv.b[i];
-	rv.b[i] = rv.b[bytes - 1 - i];
-	rv.b[bytes - 1 - i] = tmp;
+        uint8_t tmp = rv.b[i];
+        rv.b[i] = rv.b[bytes - 1 - i];
+        rv.b[bytes - 1 - i] = tmp;
     }
 #endif
     return rv.n;
@@ -74,7 +74,7 @@ uint64_t MPK_UnsafeBytesToNum(uint8_t *v, size_t bytes){
 #define MPK_ISBLOB(t) (MPK_ISEXT(t)||MPK_ISSTR(t)||MPK_ISBIN(t))
 
 //numerics
-#define MPK_ISNUM(t) \
+#define MPK_ISNUM(t)                                            \
     (MPK_ISUF(t)||MPK_ISIF(t)||IN_RANGEI(MPK_F32, t, MPK_I64))
 
 //Const
@@ -87,13 +87,13 @@ int MPK_STSiz(uint8_t t){
     if(MPK_ISUF(t) || MPK_ISIF(t) || MPK_ISBOOL(t) || MPK_ISNIL(t)) return 1;
     switch(t){
     case MPK_U8: case MPK_I8:
-	return 2;
+        return 2;
     case MPK_U16: case MPK_I16:
-	return 3;
+        return 3;
     case MPK_U32: case MPK_I32: case MPK_F32:
-	return 5;
+        return 5;
     case MPK_U64: case MPK_I64: case MPK_F64:
-	return 9;
+        return 9;
     }
     return 0;
 }
@@ -107,22 +107,22 @@ int MPK_STSiz(uint8_t t){
 //and an error pointer. error will be set to 1 if the requested
 //number overflows the end of the message.
 
-#define MPKNUM(NAME, CTYPE, bytes)				\
-    CTYPE MPK_Num ## NAME (mpk_msg_t *a, size_t off, int *err){	\
-	uint8_t *num;						\
-	if((num = INDEX(a->msg, off + 1, a->size))		\
-	   && INDEX(a->msg, off + bytes, a->size)){		\
-	    union {uint64_t in; CTYPE out;} rv;			\
-	    rv.in = MPK_UnsafeBytesToNum(num, bytes);		\
-	    return rv.out;					\
-	}else{							\
-	    *err = 1;						\
-	    return 0;						\
-	}							\
-    }								\
+#define MPKNUM(NAME, CTYPE, bytes)                              \
+    CTYPE MPK_Num ## NAME (mpk_msg_t *a, size_t off, int *err){ \
+        uint8_t *num;                                           \
+        if((num = INDEX(a->msg, off + 1, a->size))              \
+           && INDEX(a->msg, off + bytes, a->size)){             \
+            union {uint64_t in; CTYPE out;} rv;                 \
+            rv.in = MPK_UnsafeBytesToNum(num, bytes);           \
+            return rv.out;                                      \
+        }else{                                                  \
+            *err = 1;                                           \
+            return 0;                                           \
+        }                                                       \
+    }                                                           \
     CTYPE MPK_Num ## NAME (mpk_msg_t *a, size_t off, int *err)
 
-MPKNUM(F32, float, 4);
+    MPKNUM(F32, float, 4);
 MPKNUM(F64, float, 8);
 MPKNUM(U8, uint8_t, 1);
 MPKNUM(U16, uint16_t, 2);
@@ -136,20 +136,20 @@ MPKNUM(I64, int64_t, 8);
 uint8_t MPK_NumUF(mpk_msg_t *a, size_t off, int *err){
     uint8_t *num;
     if((num = INDEX(a->msg, off, a->size))){
-	return *num;
+        return *num;
     }else{
-	*err = 1;
-	return 0;
+        *err = 1;
+        return 0;
     }
 }
 
 int8_t MPK_NumIF(mpk_msg_t *a, size_t off, int *err){
     uint8_t *num;
     if((num = INDEX(a->msg, off, a->size))){
-	return (int8_t)*num;
+        return (int8_t)*num;
     }else{
-	*err = 1;
-	return 0;
+        *err = 1;
+        return 0;
     }
 }
 #undef MPKNUM
@@ -169,57 +169,57 @@ mpk_blob_t MPK_Blob(mpk_msg_t *a, size_t off){
     rv.type[1] = 0;
     rv.off = 0;
     if((p = INDEX(a->msg, off, a->size))){
-	if(MPK_ISSTR(*p)) rv.type[0] = MPK_STR;
-	else if(MPK_ISBIN(*p)) rv.type[0] = MPK_BIN;
-	else if(MPK_ISEXT(*p)) rv.type[0] = MPK_EXT;
-	else return rv;
-	if(rv.type[0] == MPK_STR && MPK_ISSF(*p)){
-	    rv.size = *p & 0x1f;
-	}else if(rv.type[0] == MPK_EXT && MPK_ISXF(*p)){
-	    rv.size = 1 << (*p - MPK_XF1);
-	}else{
-	    int err = 0;
-	    switch(*p){
-	    case MPK_B8:
-	    case MPK_S8:
-	    case MPK_X8:
-		ibytes = 1;
-		rv.size = MPK_NumU8(a, off, &err);
-		break;
-	    case MPK_B16:
-	    case MPK_S16:
-	    case MPK_X16:
-		ibytes = 2;
-		rv.size = MPK_NumU16(a, off, &err);
-		break;
-	    case MPK_B32:
-	    case MPK_S32:
-	    case MPK_X32:
-		ibytes = 4;
-		rv.size = MPK_NumU32(a, off, &err);
-		break;
-	    default:
-		return rv; //Can't Happen (TM)
-	    }
-	    if(err) return rv;
-	}
+        if(MPK_ISSTR(*p)) rv.type[0] = MPK_STR;
+        else if(MPK_ISBIN(*p)) rv.type[0] = MPK_BIN;
+        else if(MPK_ISEXT(*p)) rv.type[0] = MPK_EXT;
+        else return rv;
+        if(rv.type[0] == MPK_STR && MPK_ISSF(*p)){
+            rv.size = *p & 0x1f;
+        }else if(rv.type[0] == MPK_EXT && MPK_ISXF(*p)){
+            rv.size = 1 << (*p - MPK_XF1);
+        }else{
+            int err = 0;
+            switch(*p){
+            case MPK_B8:
+            case MPK_S8:
+            case MPK_X8:
+                ibytes = 1;
+                rv.size = MPK_NumU8(a, off, &err);
+                break;
+            case MPK_B16:
+            case MPK_S16:
+            case MPK_X16:
+                ibytes = 2;
+                rv.size = MPK_NumU16(a, off, &err);
+                break;
+            case MPK_B32:
+            case MPK_S32:
+            case MPK_X32:
+                ibytes = 4;
+                rv.size = MPK_NumU32(a, off, &err);
+                break;
+            default:
+                return rv; //Can't Happen (TM)
+            }
+            if(err) return rv;
+        }
     }else{
-	return rv;
+        return rv;
     }
 
     if(rv.type[0] == MPK_EXT){
-	if((p = INDEX(a->msg, off + ibytes + 1, a->size))) //ext's typespec...
-	    rv.type[1] = (int8_t)*p;
-	else return rv;
+        if((p = INDEX(a->msg, off + ibytes + 1, a->size))) //ext's typespec...
+            rv.type[1] = (int8_t)*p;
+        else return rv;
     }
 
     //eod == End Of Data. There's an extra +1 for ext to account for typespec.
     size_t eod = off + ibytes + rv.size + ((rv.type[0] == MPK_EXT)?1:0);
     if(INDEX(a->msg, eod, a->size)){
-	//doff = data offset. See above for explanation of the +1.
-	size_t doff = off + ibytes + ((rv.type[0] == MPK_EXT)?1:0) + 1;
-	rv.data = (a->msg + doff);
-	rv.off = doff;
+        //doff = data offset. See above for explanation of the +1.
+        size_t doff = off + ibytes + ((rv.type[0] == MPK_EXT)?1:0) + 1;
+        rv.data = (a->msg + doff);
+        rv.off = doff;
     }
     return rv;
 }
@@ -232,32 +232,32 @@ mpk_collection_t MPK_Col(mpk_msg_t *a, size_t off){
     rv.msg = a;
     rv.off = 0; //data offset will never be zero.
     if((p = INDEX(a->msg, off, a->size))){
-	if(MPK_ISARR(*p)) rv.type = MPK_ARR;
-	else if(MPK_ISMAP(*p)) rv.type = MPK_MAP;
-	else return rv;
+        if(MPK_ISARR(*p)) rv.type = MPK_ARR;
+        else if(MPK_ISMAP(*p)) rv.type = MPK_MAP;
+        else return rv;
 
-	if (MPK_ISFIX(*p)) {
-	    rv.elems = *p & 0x0f;
-	}else{
-	    int numerr = 0;
-	    switch(*p){	
-	    case MPK_M16:
-	    case MPK_A16:
-		ibytes = 2;
-		rv.elems = MPK_NumU16(a, off, &numerr);
-		break;
-	    case MPK_M32:
-	    case MPK_A32:
-		ibytes = 4;
-		rv.elems = MPK_NumU32(a, off, &numerr);
-		break;
-	    default:
-		return rv;
-	    }
-	    if(numerr) return rv;
-	}
+        if (MPK_ISFIX(*p)) {
+            rv.elems = *p & 0x0f;
+        }else{
+            int numerr = 0;
+            switch(*p){ 
+            case MPK_M16:
+            case MPK_A16:
+                ibytes = 2;
+                rv.elems = MPK_NumU16(a, off, &numerr);
+                break;
+            case MPK_M32:
+            case MPK_A32:
+                ibytes = 4;
+                rv.elems = MPK_NumU32(a, off, &numerr);
+                break;
+            default:
+                return rv;
+            }
+            if(numerr) return rv;
+        }
     }else{
-	return rv;
+        return rv;
     }
     size_t data_off = off + 1 + ibytes;
     if (INDEX(a->msg, data_off, a->size)) rv.off = data_off;
@@ -266,79 +266,79 @@ mpk_collection_t MPK_Col(mpk_msg_t *a, size_t off){
 
 //this defines handlers for the different numeric types.
 //they're all the same.
-#define NUMERIC(T,slot) case MPK_ ## T: out->type = MPK_ ## T;	\
-    out->value.slot = MPK_Num ## T (a, off, &err);		\
+#define NUMERIC(T,slot) case MPK_ ## T: out->type = MPK_ ## T;  \
+    out->value.slot = MPK_Num ## T (a, off, &err);              \
     break
 
 int MPK_ElemAt(mpk_msg_t *a, uint8_t off, mpk_value_t* out){
     uint8_t *p;
     if((p = INDEX(a->msg, off, a->size))){
-	out->rawhdr = off;
-	if(MPK_ISBLOB(*p)){
-	    out->type = MPK_BLOB;
-	    out->value.blob = MPK_Blob(a, off);
-	    if(out->value.blob.off == 0) return 0;
-	}else if(MPK_ISCOL(*p)){
-	    out->type = MPK_COL;
-	    out->value.col = MPK_Col(a, off);
-	    if(out->value.col.off == 0) return 0;
-	}else if(MPK_ISBOOL(*p)){
-	    out->type = MPK_BOOL;
-	    out->value.boole = (*p==MPK_T);
-	}else if(MPK_ISNIL(*p)){
-	    out->type = MPK_NIL;
-	    out->value.nil = 0;
-	}else if(MPK_ISNUM(*p)){
-	    int err = 0;
-	    if(MPK_ISUF(*p)){
-		out->type = MPK_U8;
-		out->value.u8 = MPK_NumUF(a, off, &err);
-	    }else if(MPK_ISIF(*p)){
-		out->type = MPK_I8;
-		out->value.i8 = MPK_NumIF(a, off, &err);
-	    }else{
-		switch(*p){
-		    NUMERIC(U8, u8);
-		    NUMERIC(U16, u16);
-		    NUMERIC(U32, u32);
-		    NUMERIC(U64, u64);
-		    NUMERIC(I8, i8);
-		    NUMERIC(I16, i16);
-		    NUMERIC(I32, i32);
-		    NUMERIC(I64, i64);
-		    NUMERIC(F32, f32);
-		    NUMERIC(F64, f64);
-		default: //Can't Happen (TM)
-		    return 0;
-		} 
-	    }
-	    if(err) return 0;
-	}else return 0;
-	return 1;
+        out->rawhdr = off;
+        if(MPK_ISBLOB(*p)){
+            out->type = MPK_BLOB;
+            out->value.blob = MPK_Blob(a, off);
+            if(out->value.blob.off == 0) return 0;
+        }else if(MPK_ISCOL(*p)){
+            out->type = MPK_COL;
+            out->value.col = MPK_Col(a, off);
+            if(out->value.col.off == 0) return 0;
+        }else if(MPK_ISBOOL(*p)){
+            out->type = MPK_BOOL;
+            out->value.boole = (*p==MPK_T);
+        }else if(MPK_ISNIL(*p)){
+            out->type = MPK_NIL;
+            out->value.nil = 0;
+        }else if(MPK_ISNUM(*p)){
+            int err = 0;
+            if(MPK_ISUF(*p)){
+                out->type = MPK_U8;
+                out->value.u8 = MPK_NumUF(a, off, &err);
+            }else if(MPK_ISIF(*p)){
+                out->type = MPK_I8;
+                out->value.i8 = MPK_NumIF(a, off, &err);
+            }else{
+                switch(*p){
+                    NUMERIC(U8, u8);
+                    NUMERIC(U16, u16);
+                    NUMERIC(U32, u32);
+                    NUMERIC(U64, u64);
+                    NUMERIC(I8, i8);
+                    NUMERIC(I16, i16);
+                    NUMERIC(I32, i32);
+                    NUMERIC(I64, i64);
+                    NUMERIC(F32, f32);
+                    NUMERIC(F64, f64);
+                default: //Can't Happen (TM)
+                    return 0;
+                } 
+            }
+            if(err) return 0;
+        }else return 0;
+        return 1;
     }else return 0;
 }
-	    
+        
 #undef NUMERIC
-    // destructively advances *start n elems forward through a.
-    // returns zero on failure.
+// destructively advances *start n elems forward through a.
+// returns zero on failure.
 int MPK_FwdNElems(mpk_msg_t *a, mpk_value_t *start, size_t n){
     size_t off = start->rawhdr;
     if(off >= a->size) return 0;
     uint8_t* p;
     for(;n!=0 && off < a->size ;n--){
-	if((p = INDEX(a->msg, off, a->size))){
-	    if(MPK_STSiz(*p)){
-		off += MPK_STSiz(*p);
-	    }else if(start->type == MPK_BLOB){
-		off = start->value.blob.off + start->value.blob.size;
-	    }else if(start->type == MPK_COL){
-		off = start->value.col.off;
-		n += start->value.col.elems
-		    * ((start->value.col.type==MPK_MAP)?2:1); //MAP: 2x elems
-		}else return 1;
-	    if(!MPK_ElemAt(a, off, start)) return 0;
-	    off = start->rawhdr;
-	}
+        if((p = INDEX(a->msg, off, a->size))){
+            if(MPK_STSiz(*p)){
+                off += MPK_STSiz(*p);
+            }else if(start->type == MPK_BLOB){
+                off = start->value.blob.off + start->value.blob.size;
+            }else if(start->type == MPK_COL){
+                off = start->value.col.off;
+                n += start->value.col.elems
+                    * ((start->value.col.type==MPK_MAP)?2:1); //MAP: 2x elems
+            }else return 1;
+            if(!MPK_ElemAt(a, off, start)) return 0;
+            off = start->rawhdr;
+        }
     }
     if(off >= a->size) return 0;
     return 1;
@@ -363,43 +363,43 @@ int MPK_FindInMap(mpk_collection_t *map, mpk_value_t *k, mpk_value_t *v){
     if(k->type == MPK_COL) return 0;
     if(!MPK_ElemAt(map->msg, map->off, v)) return 0;
     for(int i=0; i < map->elems; i++){
-	if(k->type == v->type){
-	    int eq = 0;
-	    if(k->type == MPK_BLOB){
-		//we use the data pointer here
-		//because k may be an "artificial" value_t.
-		//That is, a value_t created by the caller that isn't
-		//actually inside of the msg.
-		eq = (k->value.blob.size == v->value.blob.size
-		      && !memcmp(k->value.blob.type, v->value.blob.type, 2)
-		      && !memcmp(k->value.blob.data, v->value.blob.data,
-				 k->value.blob.size));
-	    }else{
-		
-		/*MACRO*/
-#define EQUALAS(T, slot)						\
-		case T: eq = k->value.slot == v->value.slot; break
-		/*ENDMACRO*/
-		
-		switch(k->type){
-		    EQUALAS(MPK_BOOL, boole);
-		    EQUALAS(MPK_NIL, nil);
-		    EQUALAS(MPK_F32, f32);
-		    EQUALAS(MPK_F64, f64);
-		    EQUALAS(MPK_U8, u8);
-		    EQUALAS(MPK_U16, u16);
-		    EQUALAS(MPK_U32, u32);
-		    EQUALAS(MPK_U64, u64);
-		    EQUALAS(MPK_I8, i8);
-		    EQUALAS(MPK_I16, i16);
-		    EQUALAS(MPK_I32, i32);
-		    EQUALAS(MPK_I64, i64);
-		}
+        if(k->type == v->type){
+            int eq = 0;
+            if(k->type == MPK_BLOB){
+                //we use the data pointer here
+                //because k may be an "artificial" value_t.
+                //That is, a value_t created by the caller that isn't
+                //actually inside of the msg.
+                eq = (k->value.blob.size == v->value.blob.size
+                      && !memcmp(k->value.blob.type, v->value.blob.type, 2)
+                      && !memcmp(k->value.blob.data, v->value.blob.data,
+                                 k->value.blob.size));
+            }else{
+            
+                /*MACRO*/
+#define EQUALAS(T, slot)                                            \
+                case T: eq = k->value.slot == v->value.slot; break
+                /*ENDMACRO*/
+        
+                switch(k->type){
+                    EQUALAS(MPK_BOOL, boole);
+                    EQUALAS(MPK_NIL, nil);
+                    EQUALAS(MPK_F32, f32);
+                    EQUALAS(MPK_F64, f64);
+                    EQUALAS(MPK_U8, u8);
+                    EQUALAS(MPK_U16, u16);
+                    EQUALAS(MPK_U32, u32);
+                    EQUALAS(MPK_U64, u64);
+                    EQUALAS(MPK_I8, i8);
+                    EQUALAS(MPK_I16, i16);
+                    EQUALAS(MPK_I32, i32);
+                    EQUALAS(MPK_I64, i64);
+                }
 #undef EQUALAS
-	    }
-	    if(eq) return MPK_FwdNElems(map->msg, v, 1);
-	    if(!MPK_FwdNElems(map->msg, v, 2)) break;
-	}
+            }
+            if(eq) return MPK_FwdNElems(map->msg, v, 1);
+            if(!MPK_FwdNElems(map->msg, v, 2)) break;
+        }
     }
     return 0;
 }
@@ -419,8 +419,8 @@ size_t MPK_ColSize(mpk_value_t *val){
     if(val->value.col.type == MPK_MAP) ielems *= 2;
     mpk_value_t endel;
     if(!MPK_ElemAt(val->value.col.msg, val->value.col.off, &endel))
-	return 0;
+        return 0;
     if(!MPK_FwdNElems(val->value.col.msg, &endel, ielems))
-	return val->value.col.msg->size - val->rawhdr;
+        return val->value.col.msg->size - val->rawhdr;
     return endel.rawhdr - val->rawhdr;
 }
